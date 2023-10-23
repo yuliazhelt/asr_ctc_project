9b8d0d54510d9e3e74fcb33a147471e57a3ca1e5
Юлия Желтовская <juliazh2003@gmail.com>
2023-10-23 12:01:22 +0300

    Fix data dir in librispeech_dataset.py

--- hw_asr/datasets/librispeech_dataset.py
+++ hw_asr/datasets/librispeech_dataset.py
@@ -31,7 +31,7 @@ class LibrispeechDataset(BaseDataset):
         if data_dir is None:
             data_dir = ROOT_PATH / "data" / "datasets" / "librispeech"
             data_dir.mkdir(exist_ok=True, parents=True)
-        self._data_dir = data_dir
+        self._data_dir = Path(data_dir)
         if part == 'train_all':
             index = sum([self._get_or_load_index(part)
                          for part in URL_LINKS if 'train' in part], [])
